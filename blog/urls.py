42ee616abcfb3a97576996bfe187ed2a738f2eea
Hoshgeldi <hoshgeldi9777@gmail.com>
2025-11-07 16:48:50 +0300

    Add comment editing functionality: migrations, templates, URLs and build script

--- blog/urls.py
+++ blog/urls.py
@@ -44,5 +44,4 @@ urlpatterns = [
 
     path('comment/<int:comment_id>/edit/', views.edit_comment, name='edit_comment'),
     path('comment/<int:comment_id>/delete/', views.delete_comment, name='delete_comment'),
-
 ]
