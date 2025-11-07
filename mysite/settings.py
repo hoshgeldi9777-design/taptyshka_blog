ac533d56af2255c5352d7229b222fbf513c81a7c
Hoshgeldi <hoshgeldi9777@gmail.com>
2025-11-07 10:31:28 +0300

    Fix settings.py for production - update database config, static files, and debug settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -93,18 +93,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 #     'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 # }
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-if 'DATABASE_URL' in os.environ:
-    DATABASES['default'] = dj_database_url.config(
-        conn_max_age=600,
-        conn_health_checks=True,
-        ssl_require=True
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR}/db.sqlite3'
     )
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
