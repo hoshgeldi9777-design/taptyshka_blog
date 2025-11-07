21126e2a370927d774c957819371d0b036017198
Hoshgeldi <hoshgeldi9777@gmail.com>
2025-11-07 10:06:39 +0300

    Fix database configuration for PostgreSQL

--- mysite/settings.py
+++ mysite/settings.py
@@ -99,7 +99,12 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+        ssl_require=True
+    )
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
